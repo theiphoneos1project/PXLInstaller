f820a596267436a830a38fc2cf0a9bdbb9201ded
NightwindDev <81449663+NightwindDev@users.noreply.github.com>
2026-08-01 15:03:59 -0400

    Modify triplets to fix ld errors

--- triplets/arm64-osx.cmake
+++ triplets/arm64-osx.cmake
@@ -5,4 +5,7 @@ set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_DEPLOYMENT_TARGET "11.0")
 set(VCPKG_C_FLAGS "-mmacosx-version-min=11.0 -target arm64-apple-macos11.0")
 set(VCPKG_CXX_FLAGS "-mmacosx-version-min=11.0 -target arm64-apple-macos11.0")
-set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_OSX_ARCHITECTURES=arm64")
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS 
+    "-DCMAKE_OSX_ARCHITECTURES=arm64"
+    "-DCMAKE_OSX_DEPLOYMENT_TARGET=11.0"
+)
