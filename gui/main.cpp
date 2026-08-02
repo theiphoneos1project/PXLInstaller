c30894ec201ff6135f1ac03dc7147a18e043d35d
NightwindDev <81449663+NightwindDev@users.noreply.github.com>
2026-08-02 14:37:39 -0400

    linux: add usbmuxd masking

--- gui/main.cpp
+++ gui/main.cpp
@@ -2,6 +2,28 @@
 #include <wx/snglinst.h>
 #include <memory>
 
+#ifdef __linux__
+struct USBGuard {
+public:
+    USBGuard() {
+        long result = wxExecute("systemctl mask --now usbmuxd", wxEXEC_SYNC);
+        if (result == 0) {
+            m_masked = true;
+        }
+    }
+
+    ~USBGuard() {
+        if (m_masked) {
+            wxExecute("systemctl unmask --now usbmuxd", wxEXEC_SYNC);
+        }
+    }
+
+    bool DidSuccessfullyMask(void) const { return m_masked; }
+private:
+    bool m_masked = false;
+};
+#endif
+
 class App : public wxApp {
 public:
     bool OnInit() override {
@@ -14,6 +36,17 @@ public:
             wxLogError("Another instance is already running. Exiting.");
             return false;
         }
+
+#ifdef __linux__
+        m_usbmuxdGuard = std::make_unique<USBGuard>();
+        if (!m_usbmuxdGuard->DidSuccessfullyMask()) {
+            wxMessageBox(
+                "Could not stop usbmuxd. If the device is not detected, run:\nsudo systemctl mask --now usbmuxd\nbefore launching PXLInstaller.\nRun sudo systemctl unmask --now usbmuxd after finishing your session to allow normal usbmuxd operation.",
+                "Warning",
+                wxICON_WARNING
+            );
+        }
+#endif
         
         auto *frame = new MainFrame();
         frame->Show();
@@ -21,6 +54,9 @@ public:
     }
 private:
     std::unique_ptr<wxSingleInstanceChecker> m_checker;
+#ifdef __linux__
+    std::unique_ptr<USBGuard> m_usbmuxdGuard;
+#endif
 };
 
 wxIMPLEMENT_APP(App);
