89985082cee957e2bd7a95ca402c25e9a323834d
NightwindDev <81449663+NightwindDev@users.noreply.github.com>
2026-08-01 20:43:21 -0400

    Force dark mode on Windows

--- gui/main.cpp
+++ gui/main.cpp
@@ -3,6 +3,9 @@
 class App : public wxApp {
 public:
     bool OnInit() override {
+#if _WIN32
+        wxTheApp->SetAppearance(wxApp::Appearance::Dark);
+#endif
         auto *frame = new MainFrame();
         frame->Show();
         return true;
