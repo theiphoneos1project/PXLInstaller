2dbe281eb9d813c93c64dd16516779b7f3136bf5
NightwindDev <81449663+NightwindDev@users.noreply.github.com>
2026-08-08 22:25:48 -0400

    Fix thread issue on Windows

--- core/usb/mux.c
+++ core/usb/mux.c
@@ -516,7 +516,7 @@ void forward_loop(int tcp_fd, mux_session_t *session) {
 #ifdef _WIN32
     HANDLE t1, t2;
     t1 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread_tcp_to_usb, &ctx, 0, NULL);
-    t2 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread_tcp_to_usb, &ctx, 0, NULL);
+    t2 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread_usb_to_tcp, &ctx, 0, NULL);
 
     WaitForSingleObject(t1, INFINITE);
     WaitForSingleObject(t1, INFINITE);
